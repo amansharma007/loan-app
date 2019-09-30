07c02ebe4fe2d1c8c9571305544a3f019f0e6703
Aman Sharma <aman8696.56@gmail.com>
2019-09-30 15:07:16 +0530

    Made changes to the build for running properly on gh-pages

--- precache-manifest.3eebcd2e452b788b3b941ca72465a53f.js
+++ precache-manifest.3eebcd2e452b788b3b941ca72465a53f.js
@@ -1,34 +1,34 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "7e7ee87c36ff06e2e0032ce83ba82a51",
-    "url": "/index.html"
+    "url": "./index.html"
   },
   {
     "revision": "0989d74e311616ccde72",
-    "url": "/static/css/2.d83549e8.chunk.css"
+    "url": "./static/css/2.d83549e8.chunk.css"
   },
   {
     "revision": "c1dc485e0a7db5ba6657",
-    "url": "/static/css/main.b8eb4a26.chunk.css"
+    "url": "./static/css/main.b8eb4a26.chunk.css"
   },
   {
     "revision": "0989d74e311616ccde72",
-    "url": "/static/js/2.33f009f8.chunk.js"
+    "url": "./static/js/2.33f009f8.chunk.js"
   },
   {
     "revision": "c1dc485e0a7db5ba6657",
-    "url": "/static/js/main.4c330a6c.chunk.js"
+    "url": "./static/js/main.4c330a6c.chunk.js"
   },
   {
     "revision": "761ecd87052d50d55419",
-    "url": "/static/js/runtime-main.743c7f47.js"
+    "url": "./static/js/runtime-main.743c7f47.js"
   },
   {
     "revision": "b02bdc1b846fd65473922f5f62832108",
-    "url": "/static/media/element-icons.b02bdc1b.ttf"
+    "url": "./static/media/element-icons.b02bdc1b.ttf"
   },
   {
     "revision": "d2f69a92faa6fe990d2e613c358be705",
-    "url": "/static/media/element-icons.d2f69a92.woff"
+    "url": "./static/media/element-icons.d2f69a92.woff"
   }
 ]);
